263acdec7fa3d3080a27fbad7d9f45aef3981557
Hajime Hayano <hayanoh@amazon.com>
2017-08-02 15:00:17 -0700

    Improve comments, add function to avoid duplicate code

--- service/dynamodb/expression/condition.go
+++ service/dynamodb/expression/condition.go
@@ -2,7 +2,8 @@ package expression
 
 import "fmt"
 
-// ConditionMode will specify the types of the struct ConditionBuilder
+// ConditionMode will specify the types of the struct ConditionBuilder,
+// representing the different types of Conditions (i.e. And, Or, Between, ...)
 type ConditionMode int
 
 const (
@@ -16,16 +17,30 @@ const (
 	BetweenCond
 )
 
-// ConditionBuilder will represent the ConditionExpressions
+// ConditionBuilder will represent the ConditionExpressions in DynamoDB. It is
+// composed of operands (OperandBuilder) and other conditions (ConditionBuilder)
+// There are many different types of conditions, specified by ConditionMode.
+// Users will be able to call the BuildExpression() method on a ConditionBuilder
+// to create an Expression which can then be used for operation inputs into
+// DynamoDB.
+// More Information at: http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Expressions.ConditionExpressions.html
 type ConditionBuilder struct {
 	operandList   []OperandBuilder
 	conditionList []ConditionBuilder
 	Mode          ConditionMode
 }
 
-// Equal
-
-// Equal will create a ConditionBuilder. This will be the function call
+// Equal will create a ConditionBuilder with two OperandBuilders as children,
+// representing the two operands that are being compared. The resulting
+// ConditionBuilder can be used to build other Conditions or to create an
+// Expression to be used in an operation input. This will be the function call.
+//
+// Example:
+//
+// condition := expression.Equal(expression.NewPath("foo"), expression.NewValue(5))
+//
+// anotherCondition := expression.Not(condition)	// Used in another condition
+// expression, err := condition.BuildExpression()	// Used to make an Expression
 func Equal(left, right OperandBuilder) ConditionBuilder {
 	return ConditionBuilder{
 		operandList: []OperandBuilder{left, right},
@@ -34,22 +49,54 @@ func Equal(left, right OperandBuilder) ConditionBuilder {
 }
 
 // Equal will create a ConditionBuilder. This will be the method for PathBuilder
+//
+// Example:
+//
+// The following produce equivalent conditions:
+// condition := expression.Equal(expression.NewPath("foo"), expression.NewValue(5))
+// condition := expression.NewPath("foo").Equal(expression.NewValue(5))
 func (p PathBuilder) Equal(right OperandBuilder) ConditionBuilder {
 	return Equal(p, right)
 }
 
 // Equal will create a ConditionBuilder. This will be the method for
 // ValueBuilder
+//
+// Example:
+//
+// The following produce equivalent conditions:
+// condition := expression.Equal(expression.NewValue(10), expression.NewValue(5))
+// condition := expression.NewValue(10).Equal(expression.NewValue(5))
 func (v ValueBuilder) Equal(right OperandBuilder) ConditionBuilder {
 	return Equal(v, right)
 }
 
 // Equal will create a ConditionBuilder. This will be the method for SizeBuilder
+//
+// Example:
+//
+// The following produce equivalent conditions:
+// condition := expression.Equal(expression.NewPath("foo").Size(), expression.NewValue(5))
+// condition := expression.NewPath("foo").Size().Equal(expression.NewValue(5))
 func (s SizeBuilder) Equal(right OperandBuilder) ConditionBuilder {
 	return Equal(s, right)
 }
 
-// And will create a ConditionBuilder. This will be the function call
+// And will create a ConditionBuilder with more than two other Conditions as
+// children, representing logical statements that will be logically ANDed
+// together. The resulting ConditionBuilder can be used to build other
+// Conditions or to create an Expression to be used in an operation input. This
+// will be the function call.
+//
+// Example:
+//
+// condition1 := expression.Equal(expression.NewPath("foo"), expression.NewValue(5))
+// condition2 := expression.Less(expression.NewPath("bar"), expression.NewValue(2010))
+// condition3 := expression.NewPath("baz").Between(expression.NewValue(2), expression.NewValue(10))
+// andCondition := expression.And(condition1, condition2, condition3)
+//
+// anotherCondition := expression.Not(andCondition)		// Used in another condition
+// expression, err := andCondition.BuildExpression()	// Used to make an Expression
 func And(cond ...ConditionBuilder) ConditionBuilder {
 	return ConditionBuilder{
 		conditionList: cond,
@@ -58,12 +105,31 @@ func And(cond ...ConditionBuilder) ConditionBuilder {
 }
 
 // And will create a ConditionBuilder. This will be the method signature
+//
+// Example:
+//
+// The following produce equivalent conditions:
+// condition := expression.And(condition1, condition2, condition3)
+// condition := condition1.And(condition2, condition3)
 func (cond ConditionBuilder) And(right ...ConditionBuilder) ConditionBuilder {
 	right = append(right, cond)
 	return And(right...)
 }
 
-// Between will create a ConditionBuilder. This will be the function signature.
+// Between will create a ConditionBuilder with three operands as children, the
+// first operand representing the operand being compared, the second operand
+// representing the lower bound value of the first operand, and the third
+// operand representing the upper bound value of the first operand. The
+// resulting ConditionBuilder can be used to build other Conditions or to create
+// an Expression to be used in an operation input. This will be the function
+// call.
+//
+// Example:
+//
+// condition := expression.Between(expression.NewPath("foo"), expression.NewValue(2), expression.NewValue(6))
+//
+// anotherCondition := expression.Not(condition)	// Used in another condition
+// expression, err := condition.BuildExpression()	// Used to make an Expression
 func Between(ope, lower, upper OperandBuilder) ConditionBuilder {
 	return ConditionBuilder{
 		operandList: []OperandBuilder{ope, lower, upper},
@@ -72,28 +138,61 @@ func Between(ope, lower, upper OperandBuilder) ConditionBuilder {
 }
 
 // Between will create a ConditionBuilder. This will be the method signature for
-// PathBuilders. There will be a check for the amount of OperandBuilders later
-// (only should be three)
+// PathBuilders.
+//
+// Example:
+//
+// The following produce equivalent conditions:
+// condition := expression.Between(operand1, operand2, operand3)
+// condition := operand1.Between(operand2, operand3)
 func (p PathBuilder) Between(lower, upper OperandBuilder) ConditionBuilder {
 	return Between(p, lower, upper)
 }
 
 // Between will create a ConditionBuilder. This will be the method signature for
-// ValueBuilders. There will be a check for the amount of OperandBuilders later
-// (only should be three)
+// ValueBuilders.
+//
+// Example:
+//
+// The following produce equivalent conditions:
+// condition := expression.Between(operand1, operand2, operand3)
+// condition := operand1.Between(operand2, operand3)
 func (v ValueBuilder) Between(lower, upper OperandBuilder) ConditionBuilder {
 	return Between(v, lower, upper)
 }
 
 // Between will create a ConditionBuilder. This will be the method signature for
-// SizeBuilders. There will be a check for the amount of OperandBuilders later
-// (only should be three)
+// SizeBuilders.
+//
+// Example:
+//
+// The following produce equivalent conditions:
+// condition := expression.Between(operand1, operand2, operand3)
+// condition := operand1.Between(operand2, operand3)
 func (s SizeBuilder) Between(lower, upper OperandBuilder) ConditionBuilder {
 	return Between(s, lower, upper)
 }
 
 // BuildExpression will take an ConditionBuilder as input and output an
-// Expression
+// Expression which can be used in DynamoDB operational inputs (i.e.
+// UpdateItemInput, DeleteItemInput, etc) In the future, the Expression struct
+// can be used in some injection method into the input structs.
+//
+// Example:
+//
+// expr, err := someCondition.BuildExpression()
+//
+// deleteInput := dynamodb.DeleteItemInput{
+// 	ConditionExpression:				aws.String(expr.Expression),
+// 	ExpressionAttributeNames:		expr.Names,
+// 	ExpressionAttributeValues:	expr.Values,
+// 	Key: map[string]*dynamodb.AttributeValue{
+// 		"PartitionKey": &dynamodb.AttributeValue{
+// 			S: aws.String("SomeKey"),
+// 		},
+// 	},
+// 	TableName: aws.String("SomeTable"),
+// }
 func (cond ConditionBuilder) BuildExpression() (Expression, error) {
 	en, err := cond.buildCondition()
 	if err != nil {
@@ -108,14 +207,14 @@ func (cond ConditionBuilder) BuildExpression() (Expression, error) {
 	return expr, nil
 }
 
-// buildCondition will iterate over the tree of ConditionBuilders and
-// OperandBuilders and build a tree of ExprNodes
+// buildCondition will build a tree structure of ExprNodes based on the tree
+// structure of the input ConditionBuilder's child Conditions/Operands.
 func (cond ConditionBuilder) buildCondition() (ExprNode, error) {
 	switch cond.Mode {
 	case EqualCond:
 		return compareBuildCondition(cond)
 	case AndCond:
-		return boolBuildCondition(cond)
+		return compoundBuildCondition(cond)
 	case BetweenCond:
 		return betweenBuildCondition(cond)
 	}
@@ -123,27 +222,15 @@ func (cond ConditionBuilder) buildCondition() (ExprNode, error) {
 }
 
 // compareBuildCondition is the function to make ExprNodes from Compare
-// ConditionBuilders
+// ConditionBuilders. There will first be checks to make sure that the input
+// ConditionBuilder has the correct format.
 func compareBuildCondition(c ConditionBuilder) (ExprNode, error) {
-	if len(c.conditionList) != 0 {
-		return ExprNode{}, fmt.Errorf("Invalid ConditionBuilder. Expected 0 ConditionBuilders")
+	childNodes, err := buildChildNodes(c, 2, 0)
+	if err != nil {
+		return ExprNode{}, err
 	}
-
-	if len(c.operandList) != 2 {
-		return ExprNode{}, fmt.Errorf("Invalid ConditionBuilder. Expected 2 Operands")
-	}
-
-	operandExprNodes := make([]ExprNode, 0, len(c.operandList))
-	for _, ope := range c.operandList {
-		exprNodes, err := ope.BuildOperand()
-		if err != nil {
-			return ExprNode{}, err
-		}
-		operandExprNodes = append(operandExprNodes, exprNodes)
-	}
-
 	ret := ExprNode{
-		children: operandExprNodes,
+		children: childNodes,
 	}
 
 	// Create a string with special characters that can be substituted later: $c
@@ -155,28 +242,16 @@ func compareBuildCondition(c ConditionBuilder) (ExprNode, error) {
 	return ret, nil
 }
 
-// boolBuildCondition is the function to make ExprNodes from And/Or
-// ConditionBuilders
-func boolBuildCondition(c ConditionBuilder) (ExprNode, error) {
-	if len(c.conditionList) < 1 {
-		return ExprNode{}, fmt.Errorf("Invalid ConditionBuilder. Expected at least 1 Condition")
+// compoundBuildCondition is the function to make ExprNodes from And/Or
+// ConditionBuilders. There will first be checks to make sure that the input
+// ConditionBuilder has the correct format.
+func compoundBuildCondition(c ConditionBuilder) (ExprNode, error) {
+	childNodes, err := buildChildNodes(c, 0, 2)
+	if err != nil {
+		return ExprNode{}, err
 	}
-
-	if len(c.operandList) != 0 {
-		return ExprNode{}, fmt.Errorf("Invalid ConditionBuilder. Expected 0 Operands")
-	}
-
-	conditionExprNodes := make([]ExprNode, 0, len(c.conditionList))
-	for _, cond := range c.conditionList {
-		exprNodes, err := cond.buildCondition()
-		if err != nil {
-			return ExprNode{}, err
-		}
-		conditionExprNodes = append(conditionExprNodes, exprNodes)
-	}
-
 	ret := ExprNode{
-		children: conditionExprNodes,
+		children: childNodes,
 	}
 
 	// create a string with escaped characters to substitute them with proper
@@ -195,31 +270,68 @@ func boolBuildCondition(c ConditionBuilder) (ExprNode, error) {
 }
 
 // betweenBuildCondition is the function to make ExprNodes from Between
-// ConditionBuilders
+// ConditionBuilders. There will first be checks to make sure that the input
+// ConditionBuilder has the correct format.
 func betweenBuildCondition(c ConditionBuilder) (ExprNode, error) {
-	if len(c.conditionList) != 0 {
-		return ExprNode{}, fmt.Errorf("Invalid ConditionBuilder. Expected 0 ConditionBuilders")
+	childNodes, err := buildChildNodes(c, 3, 0)
+	if err != nil {
+		return ExprNode{}, err
 	}
-
-	if len(c.operandList) != 3 {
-		return ExprNode{}, fmt.Errorf("Invalid ConditionBuilder. Expected 3 Operands")
-	}
-
-	operandExprNodes := make([]ExprNode, 0, len(c.operandList))
-	for _, ope := range c.operandList {
-		exprNodes, err := ope.BuildOperand()
-		if err != nil {
-			return ExprNode{}, err
-		}
-		operandExprNodes = append(operandExprNodes, exprNodes)
-	}
-
 	ret := ExprNode{
-		children: operandExprNodes,
+		children: childNodes,
 	}
 
 	// Create a string with special characters that can be substituted later: $c
 	ret.fmtExpr = "$c BETWEEN $c AND $c"
 
 	return ret, nil
+}
+
+// buildChildNodes will check the format of the ConditionBuilder as well as
+// create the list of the child ExprNodes. This avoids duplication of code
+// amongst the various buildConditions.
+func buildChildNodes(c ConditionBuilder, numOB, numCB int) ([]ExprNode, error) {
+	if c.Mode == AndCond {
+		if len(c.conditionList) < numCB {
+			return []ExprNode{}, fmt.Errorf("Invalid ConditionBuilder. Expected at least %d Conditions", numCB)
+		}
+	} else {
+		if len(c.conditionList) != numCB {
+			return []ExprNode{}, fmt.Errorf("Invalid ConditionBuilder. Expected %d Conditions", numCB)
+		}
+	}
+
+	// This check will be needed for In Condition, but not needed right now
+	// if c.Mode == InCond {
+	// 	if len(c.operandList) < numOB {
+	// 		return []ExprNode{}, fmt.Errorf("Invalid ConditionBuilder. Expected at least %d Operands", numOB)
+	// 	}
+	// } else {
+	if len(c.operandList) != numOB {
+		return []ExprNode{}, fmt.Errorf("Invalid ConditionBuilder. Expected %d Operands", numOB)
+	}
+	//}
+
+	var childNodes []ExprNode
+	if len(c.operandList) == 0 {
+		childNodes = make([]ExprNode, 0, len(c.conditionList))
+		for _, cond := range c.conditionList {
+			en, err := cond.buildCondition()
+			if err != nil {
+				return []ExprNode{}, err
+			}
+			childNodes = append(childNodes, en)
+		}
+	} else if len(c.conditionList) == 0 {
+		childNodes = make([]ExprNode, 0, len(c.operandList))
+		for _, ope := range c.operandList {
+			en, err := ope.BuildOperand()
+			if err != nil {
+				return []ExprNode{}, err
+			}
+			childNodes = append(childNodes, en)
+		}
+	}
+
+	return childNodes, nil
 }
--- service/dynamodb/expression/operand.go
+++ service/dynamodb/expression/operand.go
@@ -9,37 +9,71 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
-// ValueBuilder will be the concrete struct that satisfies the OperandBuilder
+// ValueBuilder represents a value operand and will implement the OperandBuilder
 // interface. It will have various methods corresponding to the operations
-// supported in ConditionExpressions
+// supported by DynamoDB operations. (i.e. AND, BETWEEN, EQUALS) The underlying
+// undefined type member variable will be converted into dynamodb.AttributeValue
+// by dynamodbattribute.Marshal(in) when Expressions are created.
 type ValueBuilder struct {
 	value interface{}
 }
 
-// PathBuilder will be the concrete struct that satisfies the OperandBuilder
-// interface. It will have various methods corresponding to the operations
-// supported in ConditionExpressions
+// PathBuilder represents a path to either a top level item attribute or a
+// nested attribute. It will implement the OperandBuilder interface. It will
+// have various methods corresponding to the operations supported by DynamoDB
+// operations. (i.e. AND, BETWEEN, EQUALS)
 type PathBuilder struct {
 	path string
 }
 
-// SizeBuilder will implement OperandBuilder thus being an Operand. This
-// reflects the fact that the function Size() returns type that is used in place
-// of an Operand
+// SizeBuilder represents the output of the function size (path), which
+// evaluates to the size of the item attribute defined by path. Size builder
+// will implement OperandBuilder interface. It will have various methods
+// corresponding to the operations supported by DynamoDB operations.
+// (i.e. AND, BETWEEN, EQUALS)
 type SizeBuilder struct {
 	pb PathBuilder
 }
 
-// Expression implements the expressions in DynamoDB. DynamoDB operation inputs
-// take maps of aliases to pointers and strings to represent expressions.
+// Expression contains the map of aliases to names/values, representing the
+// ExpressionAttributeNames and ExpressionAttributeValues, that is needed in
+// order for the Expression string to be used in an operation input.
+// (i.e. UpdateItemInput, DeleteItemInput, etc)
+//
+// Example:
+//
+// let expr be an instance of Expression{}
+//
+// deleteInput := dynamodb.DeleteItemInput{
+// 	ConditionExpression:				aws.String(expr.Expression),
+// 	ExpressionAttributeNames:		expr.Names,
+// 	ExpressionAttributeValues:	expr.Values,
+// 	Key: map[string]*dynamodb.AttributeValue{
+// 		"PartitionKey": &dynamodb.AttributeValue{
+// 			S: aws.String("SomeKey"),
+// 		},
+// 	},
+// 	TableName: aws.String("SomeTable"),
+// }
 type Expression struct {
 	Names      map[string]*string
 	Values     map[string]*dynamodb.AttributeValue
 	Expression string
 }
 
-// ExprNode will be the nodes to the inward facing tree which all the
-// deduplication and aliasing will work on
+// ExprNode will be the generic nodes that will represent both Operands and
+// Conditions. The purpose of ExprNode is to be able to call an generic
+// recursive function on the top level ExprNode to be able to determine a root
+// node in order to deduplicate name aliases.
+// fmtExpr is a string that has escaped characters to refer to
+// names/values/children which needs to be aliased at runtime in order to avoid
+// duplicate values. The rules are as follows:
+// $p: Indicates that an alias of a name needs to be inserted. The corresponding
+//		 name to be aliased will be in the []names slice.
+// $v: Indicates that an alias of a value needs to be inserted. The
+//		 corresponding value to be aliased will be in the []values slice.
+// $c: Indicates that the fmtExpr of a child ExprNode needs to be inserted. The
+// 		 corresponding child node is in the []children slice.
 type ExprNode struct {
 	names    []string
 	values   []dynamodb.AttributeValue
@@ -47,36 +81,58 @@ type ExprNode struct {
 	fmtExpr  string
 }
 
-// OperandBuilder will be mainly satisfied by PathBuilder and ValueBuilder.
-// Concrete types that satisfy this interface will be referred to as an Operand
-// In select cases, other builders may satisfy this interface
+// OperandBuilder represents the idea of Operand which are building blocks to
+// DynamoDB Expressions. OperandBuilders will be children of ConditionBuilders
+// to represent a tree like structure of Expression dependencies. The method
+// BuildOperand() will create an instance of ExprNode, which is an generic
+// representation of both Operands and Conditions. BuildOperand() will mainly
+// be called recursively by the BuildExpression() method call when Expressions
+// are built from ConditionBuilders
 type OperandBuilder interface {
 	BuildOperand() (ExprNode, error)
 }
 
-// NewPath creates an Operand based off of the path entered
+// NewPath creates a PathBuilder, which implements the OperandBuilder interface.
+// NewPath will mainly be called in a pattern in order to create
+// ConditionBuilders.
+//
+// Example:
+//
+// condition := NewPath("foo").Equal(NewPath("bar"))
 func NewPath(p string) PathBuilder {
 	return PathBuilder{
 		path: p,
 	}
 }
 
-// NewValue creates an Operand based of the value entered
+// NewValue creates a ValueBuilder, which implements the OperandBuilder
+// interface. NewValue will mainly be called in a pattern in order to create
+// ConditionBuilders.
+//
+// Example:
+//
+// condition := NewPath("foo").Equal(NewValue(10))
 func NewValue(v interface{}) ValueBuilder {
 	return ValueBuilder{
 		value: v,
 	}
 }
 
-// Size returns a SizeBuilder which satisfies the OperandBuilder interface.
+// Size creates a SizeBuilder, which implements the OperandBuilder interface.
+// Size will mainly be called in a pattern in order to create ConditionBuilders.
+//
+// Example:
+//
+// condition := NewPath("foo").Size().Equal(NewValue(10))
 func (p PathBuilder) Size() SizeBuilder {
 	return SizeBuilder{
 		pb: p,
 	}
 }
 
-// BuildOperand will create the ExprNode which will be recursively
-// called in the BuildExpression operation
+// BuildOperand will create the ExprNode which is a generic representation of
+// Operands and Conditions. BuildOperand() is mainly for the BuildExpression()
+// method to call on, not for users to invoke.
 func (p PathBuilder) BuildOperand() (ExprNode, error) {
 	if p.path == "" {
 		return ExprNode{}, fmt.Errorf("BuildOperand Error: Path is empty")
@@ -117,8 +173,9 @@ func (p PathBuilder) BuildOperand() (ExprNode, error) {
 	return ret, nil
 }
 
-// BuildOperand will create the ExprNode which will be recursively
-// called in the BuildExpression operation
+// BuildOperand will create the ExprNode which is a generic representation of
+// Operands and Conditions. BuildOperand() is mainly for the BuildExpression()
+// method to call on, not for users to invoke.
 func (v ValueBuilder) BuildOperand() (ExprNode, error) {
 	expr, err := dynamodbattribute.Marshal(v.value)
 	if err != nil {
@@ -133,8 +190,9 @@ func (v ValueBuilder) BuildOperand() (ExprNode, error) {
 	return ret, nil
 }
 
-// BuildOperand will create the ExprNode which will be recursively
-// called in the BuildExpression operation
+// BuildOperand will create the ExprNode which is a generic representation of
+// Operands and Conditions. BuildOperand() is mainly for the BuildExpression()
+// method to call on, not for users to invoke.
 func (s SizeBuilder) BuildOperand() (ExprNode, error) {
 	ret, err := s.pb.BuildOperand()
 	ret.fmtExpr = "size (" + ret.fmtExpr + ")"
@@ -143,20 +201,23 @@ func (s SizeBuilder) BuildOperand() (ExprNode, error) {
 }
 
 // aliasList will keep track of all the names we need to alias in the nested
-// struct of conditions and operands. This will allow each alias to be unique
-// while deduplicating aliases.
+// struct of conditions and operands. This will allow each alias to be unique.
+// aliasList will be passed in as a pointer when buildExprNodes is called in
+// order to deduplicate all names within the tree strcuture of the ExprNodes.
 type aliasList struct {
 	namesList  []string
 	valuesList []dynamodb.AttributeValue
 }
 
-// buildExpression returns an Expression with aliasing for paths/values specified
-// by aliasList
+// buildExpression returns an Expression with aliasing for paths/values
+// specified by aliasList
 func (en ExprNode) buildExprNodes(al *aliasList) (Expression, error) {
 	if al == nil {
 		return Expression{}, fmt.Errorf("buildExprNodes Error: aliasList is nil")
 	}
 
+	// Since each ExprNode contains a slice of names, values, and children that
+	// correspond to the escaped characters, we an index to traverse the slices
 	index := struct {
 		name, value, children int
 	}{}
@@ -236,6 +297,9 @@ func (en ExprNode) buildExprNodes(al *aliasList) (Expression, error) {
 	return expr, nil
 }
 
+// aliasValue returns the corresponding alias to the dav value argument. Since
+// values are not deduplicated as of now, all values are just appended to the
+// aliasList and given the index as the alias.
 func (al *aliasList) aliasValue(dav dynamodb.AttributeValue) (string, error) {
 	// for ind, attrval := range al.valuesList {
 	// 	if reflect.DeepEqual(dav, attrval) {
@@ -254,6 +318,9 @@ func (al *aliasList) aliasValue(dav dynamodb.AttributeValue) (string, error) {
 	return fmt.Sprintf(":%d", len(al.valuesList)-1), nil
 }
 
+// aliasPath returns the corresponding alias to the argument string. The
+// argument is checked against all existing aliasList names in order to avoid
+// duplicate strings getting two different aliases.
 func (al *aliasList) aliasPath(nm string) (string, error) {
 	if al == nil {
 		return "", fmt.Errorf("aliasValue Error: aliasList is nil")
@@ -268,7 +335,7 @@ func (al *aliasList) aliasPath(nm string) (string, error) {
 	return fmt.Sprintf("#%d", len(al.namesList)-1), nil
 }
 
-// mergeExpressionMaps merges maps of multiple expressions
+// mergeExpressionMaps merges maps of multiple Expressions
 func mergeExpressionMaps(lists ...[]Expression) (Expression, error) {
 	ret := Expression{}
 	for _, list := range lists {
